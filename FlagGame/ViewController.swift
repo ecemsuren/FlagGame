9bd99030e382f41657f1c4cacb0f56ec7f6a76cf
ecemsuren <ecemsurenn@gmail.com>
2020-03-26 16:46:00 +0300

    Initial Commit

---
+++ FlagGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlagGame
+//
+//  Created by Ecem Aleyna on 26.03.2020.
+//  Copyright © 2020 Ecem Aleyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
