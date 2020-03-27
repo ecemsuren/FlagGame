05c705eb52b170eaf3a111328cfb7789e9477e5e
ecemsuren <ecemsurenn@gmail.com>
2020-03-27 18:06:29 +0300

    UI Alert was added

--- FlagGame/ViewController.swift
+++ FlagGame/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        countries += ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US","Turkey"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.brown.cgColor
+        button2.layer.borderColor = UIColor.brown.cgColor
+        button3.layer.borderColor = UIColor.brown.cgColor
+    
+        askQuestion()
     }
-
-
+    
+        func askQuestion(action: UIAlertAction! = nil){
+            
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+            
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+            
+        title = "\(countries[correctAnswer].uppercased()) - Score \(score)"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title : String
+        
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+        }
+        else{
+            title = "Wrong"
+            score += 0
+        }
+        let ac = UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title : "Continue", style : .default, handler : askQuestion))
+        present (ac, animated : true)
+    }
+    
 }
 
